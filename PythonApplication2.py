5cfc34451c5cf639c6bc0346944a643e7a99a6a7
bluedev83 <bludev83@outlook.com>
2020-04-13 15:48:45 -0400

    Add project files.

---
+++ PythonApplication2.py
@@ -0,0 +1,5 @@
+import sys
+from math import cos, sin, radians
+for i in range(360):
+    print (i, cos(radians(i)))
+print("Hello World")
---
+++ module1.py
@@ -0,0 +1,4 @@
+
+for i in range(360):
+    s = make_dot_string(i)
+    print(s)
